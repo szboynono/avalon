cd1a9734ada4ae29cabbbe8fb4fac2ef15b07394
szboynono <szboylbd@hotmail.com>
2020-07-15 22:26:12 -0400

    prevent reloading

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,19 +1,21 @@
 <template>
+<div>
   <h1>Game</h1>
+  <h2>{{store.getters.players}}</h2>
+</div>
 </template>
 <script lang="ts">
 import { onMounted } from "vue";
-import { useStore } from 'vuex';
+import { useStore } from "vuex";
+import router from "../router";
+window.onbeforeunload = function(e: any) {
+    return "Don't leave";
+};
 
 export default {
   setup() {
     const store = useStore();
-    // onMounted(() => {
-    //   // socket.on("userList", (users: string[]) => {
-    //   //   store.commit("updatePlayers", users);
-    //   //   console.log(users)
-    //   // });
-    // });
+    return {store}
   }
 };
 </script>
