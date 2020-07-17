f3518dea2d308a24829cf3c4fe30cca28ae488a6
szboynono <szboylbd@hotmail.com>
2020-07-16 20:19:13 -0400

    add get role function

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,21 +1,35 @@
 <template>
 <div>
   <h1>Game</h1>
-  <h2>{{store.getters.players}}</h2>
+  <button-group :primaryText="'What is my role'" :primaryFn="onGetRoleClick"/>
 </div>
 </template>
 <script lang="ts">
 import { onMounted } from "vue";
 import { useStore } from "vuex";
+import ButtonGroup from "@/components/ButtonGroup.vue";
 import router from "../router";
+
 window.onbeforeunload = function(e: any) {
     return "Don't leave";
 };
 
 export default {
+  components: {
+    ButtonGroup
+  },
   setup() {
     const store = useStore();
-    return {store}
+    onMounted(() => {
+      store.getters.socket.on('giveRole', (role: any) => {
+        console.log(role);
+      })
+    });
+
+    function onGetRoleClick() {
+      store.getters.socket.emit('requestRole');
+    }
+    return {store, onGetRoleClick}
   }
 };
 </script>
