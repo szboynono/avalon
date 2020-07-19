607482e912301023ea2ce29696488df5caae95de
szboynono <szboylbd@hotmail.com>
2020-07-19 15:10:08 -0400

    wait for users

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,71 +1,75 @@
 <template>
   <div>
     <h1>Room: {{store.getters.room}}</h1>
-    <h2>Name: {{store.getters.name}}</h2>
-    <v-list :items="store.getters.players"/>
-    <v-buttons v-if="owner" :primaryText="'Start Game'" :primaryFn="onStart"/>
+    <h2>Waiting for {{calcWaitingPlayers}} more players...</h2>
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <v-list :items="store.getters.players" />
+    <v-buttons v-if="owner" :primaryText="'Start Game'" :primaryFn="onStart" />
   </div>
 </template>
 
 <script lang="ts">
-import VList from '@/components/VList.vue';
+import VList from "@/components/VList.vue";
 import { useStore } from "vuex";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import router from "../router";
 import axios from "axios";
-import VButtons from '@/components/VButtons.vue';
+import VButtons from "@/components/VButtons.vue";
 
 export default {
   components: {
     VList,
     VButtons
-  }, 
+  },
   setup() {
     const store = useStore();
     const owner = ref(false);
 
+    const calcWaitingPlayers = computed(() => 5 - store.getters.players.length);
+
     const setGameInfo = () => {
       store.commit("updateRoom", router.currentRoute.value.params.id);
-      if(localStorage.getItem('name')) {
-        store.commit("updateName", localStorage.getItem('name'));
+      if (localStorage.getItem("name")) {
+        store.commit("updateName", localStorage.getItem("name"));
       }
 
       if (!store.getters.name) {
         router.push("/name");
       } else {
-        store.commit('updateSocket');
+        store.commit("updateSocket");
       }
 
-      if(store.getters.room && store.getters.name && store.getters.socket) {
+      if (store.getters.room && store.getters.name && store.getters.socket) {
         const socket = store.getters.socket;
-        socket.emit('name', store.getters.name);
-        socket.on('id', (id: any) => {
-          store.commit('updateId', id);
+        socket.emit("name", store.getters.name);
+        socket.on("id", (id: any) => {
+          store.commit("updateId", id);
         });
-        socket.on('userList', (users: string[]) => {
-          store.commit('updatePlayers', users);
-        })
-        socket.on('owner', (id: any) => {
-          if(store.getters.id === id) {
+        socket.on("userList", (users: string[]) => {
+          store.commit("updatePlayers", users);
+        });
+        socket.on("owner", (id: any) => {
+          if (store.getters.id === id) {
             owner.value = true;
           }
         });
-        socket.on('started', () => {
-          router.push('/game/' + store.getters.room)
-        })
+        socket.on("started", () => {
+          router.push("/game/" + store.getters.room);
+        });
       }
-    }
+    };
 
     onMounted(() => {
       setGameInfo();
     });
 
     const onStart = () => {
-      store.getters.socket.emit('start');
-    }
-    
+      store.getters.socket.emit("start");
+    };
 
-    return { store, owner, onStart };
+    return { store, owner, onStart, calcWaitingPlayers };
   }
 };
 </script>
