19bd1325a49e90bc93c9e2cadacdce2208903506
szboynono <szboylbd@hotmail.com>
2020-07-31 22:02:09 -0400

    add approve result logic

--- src/views/Game/ApproveMission.vue
+++ src/views/Game/ApproveMission.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <p>Should we send these people to the mission?</p>
-    <v-list :items="selectedPlayer" />
-    <v-buttons
-      :primaryText="'Approve'"
-      :primaryFn="onApproveClick"
-      :secondaryText="'Reject'"
-      :secondaryFn="onRejectClick"
-    />
+    <template v-if="!voteResult">
+        <p>Should we send these people to the mission?</p>
+        <v-list :items="selectedPlayer" />
+        <v-buttons
+          :primaryText="'Approve'"
+          :primaryFn="onApproveClick"
+          :secondaryText="'Reject'"
+          :secondaryFn="onRejectClick"
+        />
+    </template>
+    <template v-else>
+        <p>The purpose was {{voteResult.result ? 'Approved': 'Rejected'}}</p>
+        <div class="row">
+          <div class="col-6">
+            <h4 class="text-success">FOR</h4>
+            <v-list :items="voteResult.approvals" />
+          </div>
+          <div class="col-6">
+            <h4 class="text-danger">AGAINST</h4>
+            <v-list :items="voteResult.rejections" />
+          </div>
+        </div>
+    </template>
   </div>
 </template>
 <script lang="ts">
@@ -24,25 +39,27 @@ export default {
   setup() {
     const store = useStore();
     const selectedPlayer = ref([]);
+    const voteResult = ref();
     const onApproveClick = () => {
-      store.getters.socket.emit('submitVote', true);
-    }
+      store.getters.socket.emit("submitVote", true);
+    };
     const onRejectClick = () => {
-      store.getters.socket.emit('submitVote', false);
-    }
+      store.getters.socket.emit("submitVote", false);
+    };
     onMounted(() => {
       selectedPlayer.value = store.getters.players.filter(
         (player: any) => player.selected
       );
-      store.getters.socket.on('approveResult', (result: boolean) => {
-        console.log(result);
+      store.getters.socket.on("approveResult", (result: any) => {
+        voteResult.value = result;
       });
     });
     return {
       store,
       selectedPlayer,
       onApproveClick,
-      onRejectClick
+      onRejectClick,
+      voteResult
     };
   },
 };
