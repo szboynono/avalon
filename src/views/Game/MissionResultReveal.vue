46c00ca0caa50299d8e79a8d72654693f1ef276f
szboynono <szboylbd@hotmail.com>
2020-08-03 19:22:01 -0400

    add cards

--- src/views/Game/MissionResultReveal.vue
+++ src/views/Game/MissionResultReveal.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    
+  <div class="d-flex flex-wrap justify-content-center">
+    <div class="quest border border-muted" v-for="quest in questResults" :key="quest">
+      <p>{{quest}}</p>
+    </div>
   </div>
 </template>
 
 <script lang='ts'>
+import { useStore } from 'vuex';
+import { onMounted, ref } from 'vue';
 export default {
-  
+  setup() {
+    const store = useStore();
+    const questResults = ref(); 
+    onMounted(() => {
+      questResults.value = store.getters.missionSuccessResult.players.map((player: any) => {
+        return player.successMission.success ? 'Success' : 'Failure';
+      });
+    })
+    return {questResults};
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.quest {
+  width: 150px;
+  height: 200px;
+}
+</style>
