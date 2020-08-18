4cefb8510d4b35a2951e4009807efe0db77c9ad4
szboynono <szboylbd@hotmail.com>
2020-08-18 18:17:26 -0400

    cimdfa

--- src/views/Game/MissionResultReveal.vue
+++ src/views/Game/MissionResultReveal.vue
@@ -3,22 +3,31 @@
     <div class="quest border border-muted" v-for="quest in questResults" :key="quest">
       <p>{{quest}}</p>
     </div>
+    <v-buttons :primaryText="'Next Round'" :primaryFn="onNextRoundClick" />
   </div>
 </template>
 
 <script lang='ts'>
 import { useStore } from 'vuex';
 import { onMounted, ref } from 'vue';
+import VButtons from '@/components/VButtons.vue';
 export default {
+  components: {
+    VButtons
+  },
   setup() {
     const store = useStore();
     const questResults = ref(); 
+
+    const onNextRoundClick = () => {
+      store.getters.socket.emit('turnOver');
+    }
     onMounted(() => {
       questResults.value = store.getters.missionSuccessResult.players.map((player: any) => {
         return player.successMission.success ? 'Success' : 'Failure';
       });
     })
-    return {questResults};
+    return {questResults, onNextRoundClick};
   }
 }
 </script>
